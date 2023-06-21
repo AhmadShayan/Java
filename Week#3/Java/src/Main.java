53ef13c1719d48d93f4bc8b1ad8fae478c7169af
Shayan <expertshayan@gmail.com>
2023-06-21 15:03:38 +0500

    -

---
+++ Week#3/Java/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Person obj1 = new Person();
+        obj1.setName("Aadam");
+        System.out.println(obj1.getName());
+    }
+
+
+}
